bca47aa236c7108ad31990ac6ae1f07b1be73ea9
abuxton <buxton.adam@gmail.com>
2018-05-31 20:26:44 +0100

    added defaults

--- site/bootstrap/manifests/people/abuxton/params.pp
+++ site/bootstrap/manifests/people/abuxton/params.pp
@@ -1,4 +1,11 @@
 class bootstrap::people::abuxton::params{
-  $user = 'abuxton'
-  $user_home = "/Users/${user}"
+case $osfamily {
+  'Darwin': {
+    $user = 'abuxton'
+    $user_home = "/Users/${user}"
+    }
+  default{
+    fail('Unsupported OS for user!')
+    }
+  }
 }
