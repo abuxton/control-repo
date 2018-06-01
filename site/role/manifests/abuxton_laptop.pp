6c5baa8db5f8a103000ed6a41ea6ea86719683fd
Adam Buxton <buxton.adam@gmail.com>
2018-06-01 19:06:14 +0100

    syntax update

--- site/role/manifests/abuxton_laptop.pp
+++ site/role/manifests/abuxton_laptop.pp
@@ -1,10 +1,10 @@
 class role::abuxton_laptop{
-  case $hostname: {
+  case $hostname {
     'home-abuxton-mba': {
       include profile::abuxton::osx_base
       include profile::abuxton::osx_puppet
     }
-    '^puppet':{
+    '^puppet': {
       include profile::abuxton::osx_base
       include profile::abuxton::osx_puppet
     }
