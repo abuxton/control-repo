0af1bf423f9c4f9f5e4757495b1603cc3ed8011e
Adam Buxton <buxton.adam@gmail.com>
2018-06-06 22:29:50 +0100

    adding some tasks

---
+++ site/bootstrap/tasks/puppet_download.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# Puppet Task Name:
+#
+# This is where you put the shell code for your task.
+#
+# You can write Puppet tasks in any language you want and it's easy to
+# adapt an existing Python, PowerShell, Ruby, etc. script. Learn more at:
+# https://puppet.com/docs/bolt/0.x/writing_tasks.html
+#
+# Puppet tasks make it easy for you to enable others to use your script. Tasks
+# describe what it does, explains parameters and which are required or optional,
+# as well as validates parameter type. For examples, if parameter "instances"
+# must be an integer and the optional "datacenter" parameter must be one of
+# portland, sydney, belfast or singapore then the .json file
+# would include:
+#   "parameters": {
+#     "instances": {
+#       "description": "Number of instances to create",
+#       "type": "Integer"
+#     },
+#     "datacenter": {
+#       "description": "Datacenter where instances will be created",
+#       "type": "Enum[portland, sydney, belfast, singapore]"
+#     }
+#   }
+# Learn more at: https://puppet.com/docs/bolt/0.x/writing_tasks.html#ariaid-title11
+#
+# This script will download the requested version of PE from S3.
+# If no version is specified, the latest version will be used. It will
+# also resume broken downloads to save time and rename the resultant file.
+
+# INSTALLER CHOICES #
+# Either pass these environment variables inline or modify the default
+# values (note, it's the value after the ':-' but before the close curly brace }
+DOWNLOAD_DIST=${DOWNLOAD_DIST:-el}
+DOWNLOAD_RELEASE=${DOWNLOAD_RELEASE:-7}
+DOWNLOAD_ARCH=${DOWNLOAD_ARCH:-x86_64}
+DOWNLOAD_VERSION=${DOWNLOAD_VERSION:-latest}
+
+if [[ $DOWNLOAD_VERSION == latest ]]; then
+  latest_released_version_number="$(curl -s http://versions.puppet.com.s3-website-us-west-2.amazonaws.com/ | tail -n1)"
+  DOWNLOAD_VERSION=${latest_released_version_number:-latest}
+fi
+
+tarball_name="puppet-enterprise-${DOWNLOAD_VERSION}-${DOWNLOAD_DIST}-${DOWNLOAD_RELEASE}-${DOWNLOAD_ARCH}.tar.gz"
+
+echo "Downloading PE $DOWNLOAD_VERSION for ${DOWNLOAD_DIST}-${DOWNLOAD_RELEASE}-${DOWNLOAD_ARCH} to: ${tarball_name}"
+echo
+
+curl --progress-bar \
+  -L \
+  -o "./${tarball_name}" \
+  -C - \
+  "https://pm.puppetlabs.com/cgi-bin/download.cgi?dist=${DOWNLOAD_DIST}&rel=${DOWNLOAD_RELEASE}&arch=${DOWNLOAD_ARCH}&ver=${DOWNLOAD_VERSION}"
