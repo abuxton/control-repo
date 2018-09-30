6b67bcacad6f4ea26ca62aa393d34343a9822db3
Adam Buxton <buxton.adam@gmail.com>
2018-09-30 16:34:57 +0100

    module update name

--- site/localbootstrap/manifests/people/abuxton/osx/puppet.pp
+++ site/localbootstrap/manifests/people/abuxton/osx/puppet.pp
@@ -5,21 +5,29 @@ class localbootstrap::people::abuxton::osx::puppet(
 
   ) inherits localbootstrap::people::abuxton::params {
 
-  include resource_api::agent #required as the module is downloaded and will error on --types flag if not installed.
+  #include resource_api::agent #required as the module is downloaded and will error on --types flag if not installed.
 
   file { "${user_home}/.puppetlabs":
     ensure => 'directory',
+    tag => 'bootstrap',
   }
   file{"${user_home}/.puppetlabs/etc/code/environments":
     ensure  => 'directory',
     mode    => '0755',
     owner   => $user,
     require => File["${user_home}/.puppetlabs"],
+    tag => 'bootstrap',
   }
-  class{'pdk':
-    pdk_download_url =>  'pm.puppet.com/cgi-bin/pdk_download.cgi?dist=osx&rel=10.12&arch=x86_64',
-    pdk_version      => 'latest',
-    }
+  file { "${user_home}/.puppet_bashrc":
+    ensure => 'link',
+    mode   => '0755',
+    target => '/Users/abuxton/Dropbox/profile/dotfiles/puppet_bashrc',
+    tag => 'bootstrap',
+  }
+    # class{'pdk':
+    #   pdk_download_url =>  'pm.puppet.com/cgi-bin/pdk_download.cgi?dist=osx&rel=10.12&arch=x86_64',
+    #   pdk_version      => 'latest',
+    #   }
 
 
 }
