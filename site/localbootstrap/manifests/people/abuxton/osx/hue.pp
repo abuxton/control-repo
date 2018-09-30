c2dcea4807693d06a45b0f6788393f89c404b3cb
Adam Buxton <buxton.adam@gmail.com>
2018-09-30 16:52:56 +0100

    hue

--- site/localbootstrap/manifests/people/abuxton/osx/hue.pp
+++ site/localbootstrap/manifests/people/abuxton/osx/hue.pp
@@ -3,25 +3,32 @@
 # @summary A short summary of the purpose of this class
 #
 # @example
-#   include bootstrap::localbootstrap::people::abuxton::osx::hue
+#   include localbootstrap::people::abuxton::osx::hue
 class localbootstrap::people::abuxton::osx::hue(
-  my_hue = '192.168.1.21',
-  ) {
+    $my_hue = '192.168.1.21',
+    $my_hue_key = 'wjAhNpeyJCJwsFEHUUkU7GUvk1M5D-Mwa0sEO9Tp' ,
+  ) inherits localbootstrap::people::abuxton::params{
+
 
   file { "${user_home}/.hue_bash_profile":
     ensure => 'file',
     mode   => '0755',
     #target => '/Users/abuxton/Dropbox/profile/dotfiles/hue_bash_profile',
     tag => 'bootstrap',
-    content => '
-    export HUE_KEY=pUAUbcowosjBKgE5VQTrD6kkfkfoQK1ZtN14pHl8
-    export HUE_IP=192.168.1.21'
+    content => "
+    export HUE_KEY=$my_hue_key
+    export HUE_IP=$my_hue
+    "
   }
   host { 'my-hue.localdomain':
   ensure       => 'present',
   host_aliases => ['my-hue'],
   ip           => $my_hue,
   target       => '/etc/hosts',
+}
+file_line { 'source hue_bash_profile':
+   path => "${user_home}/.bash_profile",
+   line => 'source ~/.hue_bash_profile',
 }
 
 }
