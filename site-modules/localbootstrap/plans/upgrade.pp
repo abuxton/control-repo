2ce2f130dd10bf481d5bbe0a8ccc35c4f91a8f78
Adam Buxton <buxton.adam@gmail.com>
2019-04-24 22:02:13 +0100

    return

--- site-modules/localbootstrap/plans/upgrade.pp
+++ site-modules/localbootstrap/plans/upgrade.pp
@@ -1,5 +1,15 @@
 plan localbootstrap::upgrade(
   TargetSpec $nodes,
+  String tmpdir,
 ){
-  run_task('localbootstrap::puppet_download', $nodes)
+  $r = run_task('localbootstrap::puppet_download', $nodes)
+  $r.each |$result| {
+  $node = $result.target.name
+  if $result.ok {
+    notice("${node} returned a value: ${result.value}")
+  } else {
+    notice("${node} errored with a message: ${result.error.message}")
+  }
+}
+  #run_task('localbootstrap::upgrade',$nodes, pe_version =>,tmpdir=>,'_run_as'=> 'root')
 }
